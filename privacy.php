4ee49e1ce8e15fb1f5f5e10f6e05044a2e88cfe6
Kailash Patel <patelkailash200@gmail.com>
2022-03-02 20:01:17 +0530

    final commit

--- privacy.php
+++ privacy.php
@@ -12,13 +12,13 @@
 <body>
     <h1>Understanding Privacy at iMusic</h1>
     <p>
-    At Spotify, we want to give you the best possible experience. To do this we use some personal data about you to provide your personalized Spotify service and to develop the best audio platform for our listeners and creators. We are also committed to protecting the privacy and security of your data. Check out the below privacy resources and watch the video on this page to learn more.
+    At iMusic, we want to give you the best possible experience. To do this we use some personal data about you to provide your personalized iMusic service and to develop the best audio platform for our listeners and creators. We are also committed to protecting the privacy and security of your data. Check out the below privacy resources and watch the video on this page to learn more.
     <br>
     <ol>
         <li> Our Privacy Center is a user-friendly hub that explains the key elements of our Privacy Policy - the data we collect, how we use and protect it, and your rights and controls over it. </li>
         <li> Our full Privacy Policy contains all the important details. </li>
-        <li> Our Privacy & Security Spotify Support pages provide extra support and answers to frequently asked questions. </li>
-        <li> Our guide to Spotify for parents and guardians provides advice on how to support and protect your teenage or younger child when using Spotify’s services. </li>
+        <li> Our Privacy & Security iMusic Support pages provide extra support and answers to frequently asked questions. </li>
+        <li> Our guide to iMusic for parents and guardians provides advice on how to support and protect your teenage or younger child when using iMusic’s services. </li>
         <li> If you’re still looking for help, you can <a href="./contactUs.php">contact us</a>. </li>
     </ol>
     </p>
@@ -30,16 +30,16 @@
     <br>
     <ol>
         <li>
-            <b> Personal data collected when you sign up for the Spotify Service or when you update your account</b> - we collect certain personal data to create your Spotify account so you can use the Spotify Service. This includes your User Data and Street Address Data, as described in more detail in section 3 of our Privacy Policy.
+            <b> Personal data collected when you sign up for the iMusic Service or when you update your account</b> - we collect certain personal data to create your iMusic account so you can use the iMusic Service. This includes your User Data and Street Address Data, as described in more detail in section 3 of our Privacy Policy.
         </li>
         <li>
-            <b> Personal data collected through your use of the Spotify Service</b> - when you use or access the Spotify Service, we collect personal data about your actions. This includes the songs you have played and playlists you have created. This is the Usage Data category contained in section 3 of our Privacy Policy.
+            <b> Personal data collected through your use of the iMusic Service</b> - when you use or access the iMusic Service, we collect personal data about your actions. This includes the songs you have played and playlists you have created. This is the Usage Data category contained in section 3 of our Privacy Policy.
         </li>
         <li>
             <b> Personal data that you choose to give us</b> - from time to time, you may also provide us with additional personal data or give us permission to collect personal data e.g. to provide you with more features or functionality. These include Voice Data, Payment and Purchase Data and Contests, Surveys and Sweepstakes Data categories contained in section 3 of our Privacy Policy.
         </li>
         <li>
-            <b> Personal data we collect from third party sources</b> - if you sign up for Spotify using another service or connect your Spotify account to a third party application, service or device, we’ll receive your data from those third parties. We may also receive your data from technical service providers, payment partners and advertising and marketing partners. See section 3 of the Privacy Policy for more details.
+            <b> Personal data we collect from third party sources</b> - if you sign up for iMusic using another service or connect your iMusic account to a third party application, service or device, we’ll receive your data from those third parties. We may also receive your data from technical service providers, payment partners and advertising and marketing partners. See section 3 of the Privacy Policy for more details.
         </li>
     </ol>
     </p>
@@ -49,13 +49,13 @@
     <br>
     <ul>
         <li>
-            to provide the personalized Spotify Service
+            to provide the personalized iMusic Service
         </li>
         <li>
-            to understand, diagnose, troubleshoot, and fix issues with the Spotify Service
+            to understand, diagnose, troubleshoot, and fix issues with the iMusic Service
         </li>
         <li>
-            to evaluate and develop new features, technologies, and improvements to the Spotify Service
+            to evaluate and develop new features, technologies, and improvements to the iMusic Service
         </li>
         <li>
             for marketing, promotion, and advertising purposes
@@ -64,7 +64,7 @@
             to comply with legal obligations and law enforcement requests
         </li>
         <li>
-            to fulfill contractual obligations with third parties, for example our agreements with owners of content on the Spotify Service, and take appropriate action with reports of intellectual property infringement and inappropriate content
+            to fulfill contractual obligations with third parties, for example our agreements with owners of content on the iMusic Service, and take appropriate action with reports of intellectual property infringement and inappropriate content
         </li>
         <li>
             to establish, exercise, or defend legal claims
@@ -76,7 +76,7 @@
             to conduct business planning, reporting, and forecasting
         </li>
         <li>
-            to detect and prevent fraud, including fraudulent payments and fraudulent use of the Spotify Service
+            to detect and prevent fraud, including fraudulent payments and fraudulent use of the iMusic Service
         </li>
         <li>
             to conduct research, contests, surveys, and sweepstakes
